5e2224229ca70d46b6cc9edec30801675cc67b56
MarkusEson <31851344+MarkusEson@users.noreply.github.com>
2017-09-14 13:13:48 +0200

    Add files via upload

--- JanuariYahtzee/yahtzeemainwin.cpp
+++ JanuariYahtzee/yahtzeemainwin.cpp
@@ -3,26 +3,34 @@
 #include <QDebug>
 #include <QAbstractButton>
 
+
 YahtzeeMainWin::YahtzeeMainWin(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::YahtzeeMainWin)
 {
     ui->setupUi(this);
 
-    // connect all buttons
-    // add graphics scene OPTIONS menu
-
-    for(int i = 0; i < ui->Agrid->count(); i++)
-    {
-
+    /*
+     * En funktion som connectar alla knappar i layouterna Agrid, Bgrid, Cgrid och Dgrid.
+     * Funktionen är inspirerad från GUI labbarna samt stackexchange sidan https://stackoverflow.com/questions/4065378/qt-get-children-from-layout
+     *
+     */
+    for(int i = 0; i < ui->Agrid->count(); i++){
         QWidget *button = ui->Agrid->itemAt(i)->widget();
         connect(button, SIGNAL(clicked()), this, SLOT(aButtonWasClicked()));
     }
-
-    ui->A7->setEnabled(false);
-    ui->A8->setEnabled(false);
-    ui->A19->setEnabled(false);
-
+    for(int i = 0; i < ui->Bgrid->count(); i++){
+        QWidget *button = ui->Bgrid->itemAt(i)->widget();
+        connect(button, SIGNAL(clicked()), this, SLOT(aButtonWasClicked()));
+    }
+    for(int i = 0; i < ui->Cgrid->count(); i++){
+        QWidget *button = ui->Cgrid->itemAt(i)->widget();
+        connect(button, SIGNAL(clicked()), this, SLOT(aButtonWasClicked()));
+    }
+    for(int i = 0; i < ui->Dgrid->count(); i++){
+        QWidget *button = ui->Dgrid->itemAt(i)->widget();
+        connect(button, SIGNAL(clicked()), this, SLOT(aButtonWasClicked()));
+    }
 
 
 }
@@ -32,14 +40,124 @@ YahtzeeMainWin::~YahtzeeMainWin()
     delete ui;
 }
 
+
+void YahtzeeMainWin::optionsButtonClicked()
+{
+    if(ui->menuLabel->isHidden()){
+        for(int i = 0; i < ui->menuLayout->count(); i++){
+             QWidget *widg = ui->menuLayout->itemAt(i)->widget();
+             widg->show();
+        }
+        ui->optionsBackground->show();
+    }
+    else{
+        for(int i = 0; i < ui->menuLayout->count(); i++){
+             QWidget *widg = ui->menuLayout->itemAt(i)->widget();
+             widg->hide();
+        }
+        ui->optionsBackground->hide();
+    }
+}
+
+void YahtzeeMainWin::showPlayerBlockersOnClick()
+{
+    // Shows the playerBlockers on click.
+    ui->playerBlockerA->show();
+    ui->playerBlockerB->show();
+    ui->playerBlockerC->show();
+    ui->playerBlockerD->show();
+}
+
+void YahtzeeMainWin::chooseAmountOfPlayers(int num)
+{
+    switch(num)
+    {
+    case 1:{
+        // First turns on the player blocker windows, then hides them according to how many players are playing.
+        showPlayerBlockersOnClick();
+        optionsButtonClicked();
+        ui->playerBlockerA->hide();
+        break;
+    }
+    case 2:{
+        showPlayerBlockersOnClick();
+        optionsButtonClicked();
+        ui->playerBlockerA->hide();
+        ui->playerBlockerB->hide();
+        break;
+    }
+    case 3:{
+        showPlayerBlockersOnClick();
+        optionsButtonClicked();
+        ui->playerBlockerA->hide();
+        ui->playerBlockerB->hide();
+        ui->playerBlockerC->hide();
+        break;
+    }
+    case 4:{
+        showPlayerBlockersOnClick();
+        optionsButtonClicked();
+        ui->playerBlockerA->hide();
+        ui->playerBlockerB->hide();
+        ui->playerBlockerC->hide();
+        ui->playerBlockerD->hide();
+        break;
+    }
+    }
+
+}
+
+
+
 void YahtzeeMainWin::aButtonWasClicked()
 {
     QPushButton *theButton = dynamic_cast<QPushButton*>(sender());
 
-    qDebug() << "FUNKTIONENENENENN";
+    //qDebug() << "FUNKTIONENENENENN";
 
     theButton->setText("hej");
     theButton->setEnabled(false);
 
     ui->A7->setText("sum");
+
+    /*
+    if(theButton == ui->A1)
+    {
+        ui->A1->setChecked(true);
+    }
+    */
+}
+
+void YahtzeeMainWin::on_onePlayerButton_clicked()
+{
+    _numOfPlayers = 1;
+    chooseAmountOfPlayers(_numOfPlayers);
+}
+
+void YahtzeeMainWin::on_twoPlayerButton_clicked()
+{
+    _numOfPlayers = 2;
+    chooseAmountOfPlayers(_numOfPlayers);
+}
+
+void YahtzeeMainWin::on_threePlayerButton_clicked()
+{
+    _numOfPlayers = 3;
+    chooseAmountOfPlayers(_numOfPlayers);
+}
+
+void YahtzeeMainWin::on_fourPlayerButton_clicked()
+{
+    _numOfPlayers = 4;
+    chooseAmountOfPlayers(_numOfPlayers);
+}
+
+void YahtzeeMainWin::on_optionsButton_clicked()
+{
+    optionsButtonClicked();
+}
+
+void YahtzeeMainWin::on_rollDiceButton_clicked()
+{
+
 }
